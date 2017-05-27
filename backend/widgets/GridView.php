6027d3c0875b4e63c0a1ba437dd2fd7ae6acf5ce
wangtao <yzxh24@gmail.com>
2017-05-27 20:57:28 +0800

    fix pagination bug

--- backend/widgets/GridView.php
+++ backend/widgets/GridView.php
@@ -7,6 +7,9 @@ namespace backend\widgets;
  * Date: 2017/3/28
  * Time: 17:44
  */
+
+use Yii;
+
 class GridView extends \yii\grid\GridView
 {
     public $layout = "{items}\n<div class='row'><div class=\"col-sm-5\"><div class='dataTables_info'>{summary}</div></div>\n<div class=\"col-sm-7\"><div class=\"dataTables_paginate paging_simple_numbers\">{pager}</div></div></div>";
@@ -23,4 +26,14 @@ class GridView extends \yii\grid\GridView
     public $options = ["class" => "grid-view", "id" => "grid"];
 
     public $id = 'id';
+
+    public function init()
+    {
+        parent::init();
+
+        $adminltePlugin = Yii::getAlias('@vendor/almasaeed2010/adminlte/plugins');
+        $push = Yii::$app->assetManager->publish($adminltePlugin . '/datatables/');
+
+        $this->view->registerCssFile($push[1] . '/dataTables.bootstrap.css', ['depends' => ['yii\bootstrap\BootstrapAsset']]);
+    }
 }
